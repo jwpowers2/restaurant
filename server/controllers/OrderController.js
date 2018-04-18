88777e55e3294b42dfac857386867681f36ede73
John Powers <john.powers070@gmail.com>
2018-04-18 19:38:11 -0400

    clean up order control class

--- server/controllers/OrderController.js
+++ server/controllers/OrderController.js
@@ -6,23 +6,36 @@ let Restaurant = require("mongoose").model("Restaurant");
 class OrderController{
   
   create(req,res){
+
   	User.findOne({_id:req.body.user},(err,user)=>{
   		
   		Restaurant.findOne({name:req.body.restaurant},(err,restaurant)=>{
+
   			var order = new Order(req.body);
   			order.restaurant = restaurant._id;
   			restaurant.orders.push(order);
             user.orders.push(order);
+
             order.save((err)=>{
+
             	restaurant.save((err)=>{
+
             		if(err){
+
             			console.log("error");
+
             		} else {
+
             			user.save((err)=>{
+
             				if(err){
+
             					console.log("error");
+
             				} else{
+
             					res.json({"order_saved":order});
+
             				}
             			});
             		}
@@ -40,23 +53,32 @@ class OrderController{
   	  User.findOne({_id:order.user},(err,user)=>{
   		
   		Restaurant.findOne({_id:order.restaurant},(err,restaurant)=>{
-  			//var order = new Order(req.body);
-  			//order.restaurant = restaurant._id;
-  			//restaurant.orders.push(order);
+  			
   			restaurant.orders = _.reject(restaurant.orders, function(el) { return el._id === req.body.id});
-            //user.orders.push(order);
             user.orders = _.reject(user.orders, function(el) { return el._id === req.body.id});
+
             Order.remove((err)=>{
+
             	restaurant.save((err)=>{
+
             		if(err){
+
             			console.log("error");
+
             		} else {
+
             			user.save((err)=>{
+
             				if(err){
+
             					console.log("error");
+
             				} else{
+
             					res.json({order_deleted:req.body.id});
+
             				}
+
             			});
             		}
             	});
@@ -71,12 +93,14 @@ class OrderController{
   read(req,res){
 
     Order.findOne({_id:req.params.id},(err,order)=>{
+
     	if(!order){
     		res.json({"errors":"order not found"});
     	} else {
     		res.json({order:order});
     	}
-    })
+
+    });
 
   }
 
