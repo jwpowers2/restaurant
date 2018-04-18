f4c33d78956898f3f4325321e180a4528243d909
John Powers <john.powers070@gmail.com>
2018-04-18 18:31:54 -0400

    add create for orders

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,12 +1,14 @@
 let UserController = require("../controllers/UserController.js");
 let ReviewController = require("../controllers/ReviewController.js");
 let RestaurantController = require("../controllers/RestaurantController");
+let OrderController = require("../controllers/OrderController");
 
 module.exports = (app)=>{
 
   app.post("/users/login",UserController.login);
   app.post("/users/register",UserController.register);
   app.post("/reviews",ReviewController.create);
+  app.post("/orders",OrderController.create);
   //app.delete("/reviews/:id",ReviewController.destroy);
   app.post("/restaurants",RestaurantController.create);
   app.delete("/restaurants",RestaurantController.destroy);
--- server/controllers/OrderController.js
+++ server/controllers/OrderController.js
@@ -1,3 +1,36 @@
 let Order = require("mongoose").model("Order");
 let User = require("mongoose").model("User");
 let Restaurant = require("mongoose").model("Restaurant");
+
+class OrderController{
+  
+  create(req,res){
+  	User.findOne({_id:req.body.user},(err,user)=>{
+  		
+  		Restaurant.findOne({name:req.body.restaurant},(err,restaurant)=>{
+  			var order = new Order(req.body);
+  			order.restaurant = restaurant._id;
+  			restaurant.orders.push(order);
+            user.orders.push(order);
+            order.save((err)=>{
+            	restaurant.save((err)=>{
+            		if(err){
+            			console.log("error");
+            		} else {
+            			user.save((err)=>{
+            				if(err){
+            					console.log("error");
+            				} else{
+            					res.json({"order_saved":order});
+            				}
+            			});
+            		}
+            	});
+            });
+  		});
+  		
+    });
+  }
+}
+
+module.exports = new OrderController();
